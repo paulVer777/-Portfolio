a5ebc7866b8817459c7b19ce9042a1afa82d3a15
paulVer777 <pawel.gitara@onet.eu>
2018-06-10 14:32:25 +0200

    efects added

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,14 +1,30 @@
-
 //////TOP BUTTON/////////////////////////////////////
 
 
-window.addEventListener('scroll',()=> {
-    const button= $('.top-button');
+window.addEventListener('scroll', () => {
+    const button = $('.top-button');
     if (scrollY > 477) button.fadeIn(500);
-   else button.fadeOut(500)
+    else button.fadeOut(500)
 });
 
 //////////////////////////////////////////////////
+let count=0;
+
+const crazy = () => {
+
+    const icon = $('.header__icon')
+    count=count+1;
+
+    if(count===360) count=0;
+
+    icon.css('transform', `rotate(${count}deg)`)
+console.log(count)
+
+
+}
+
+setInterval(crazy,50)
+
 
 
 
