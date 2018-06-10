6193023e15982342b1e58aea88a6f68a477f71f6
paulVer777 <pawel.gitara@onet.eu>
2018-06-10 12:52:23 +0200

    topd button added

---
+++ js/javaScript.js
@@ -0,0 +1,14 @@
+
+//////TOP BUTTON/////////////////////////////////////
+
+
+window.addEventListener('scroll',()=> {
+    const button= $('.top-button');
+    if (scrollY > 477) button.fadeIn(500);
+   else button.fadeOut(500)
+});
+
+//////////////////////////////////////////////////
+
+
+
